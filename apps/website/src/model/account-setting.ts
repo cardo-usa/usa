c95ae8d16ff08d766b2e26231e360639551d5b6a
shio <85730998+dino3616@users.noreply.github.com>
2023-08-15 01:06:55 +0900

    feat: ✨ (website/model) AccountSetting model を作成

---
+++ apps/website/src/model/account-setting.ts
@@ -0,0 +1,7 @@
+type IconBackgroundColor = 'tomato' | 'violet' | 'indigo' | 'cyan' | 'green' | 'orange' | 'brown' | 'amber';
+
+export type AccountSetting = {
+  iconEmoji: string;
+  iconBackgroundColor: IconBackgroundColor;
+  name: string;
+};
