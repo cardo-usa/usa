56ce4d5c9c193566c70479909bfc9edb0fd667f2
shio <85730998+dino3616@users.noreply.github.com>
2023-08-14 16:33:36 +0900

    feat: 🎈 (core/font) design-tokenを参照するように修正

--- packages/core/font/family.ts
+++ packages/core/font/family.ts
@@ -1,15 +1,12 @@
-import { Fira_Code, Noto_Sans } from 'next/font/google';
+import type { fonts } from '@usa/design-token';
+import { Noto_Sans } from 'next/font/google';
 
-const firaCode = Fira_Code({
+// HACK: Since SWC forces argument properties to be written in literals, fonts cannot be used interchangeably.
+// Therefore, the consistency of variable is only ensured by generics.
+const notoSans = Noto_Sans<(typeof fonts)['noto-sans']['variable']>({
   weight: ['400', '700'],
   subsets: ['latin'],
-  variable: '--font-fira-code',
+  variable: '--noto-sans',
 });
 
-const notoSans = Noto_Sans({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  variable: '--font-noto-sans',
-});
-
-export const fontFamily = [firaCode, notoSans].map((font) => font.variable).join(' ');
+export const fontFamily = [notoSans].map((font) => font.variable).join(' ');
