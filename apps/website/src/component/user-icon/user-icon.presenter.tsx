28b051dd8281cdc7a6e7afde4b0586de5e5c1b7d
shio <85730998+dino3616@users.noreply.github.com>
2023-08-15 10:51:53 +0900

    refactor: ♻️ (user-icon) 大きさが fontSize のみに依存するように修正

--- apps/website/src/component/user-icon/user-icon.presenter.tsx
+++ apps/website/src/component/user-icon/user-icon.presenter.tsx
@@ -15,18 +15,18 @@ const userIconVariant = tv({
       amber: 'bg-amber-9',
     } satisfies Record<AccountSetting['iconBackgroundColor'], string>,
     size: {
-      sm: 'h-6 w-6 text-sm',
-      md: 'h-6 w-6 text-base',
-      lg: 'h-7 w-7 text-lg',
-      xl: 'h-7 w-7 text-xl',
-      '2xl': 'h-8 w-8 text-2xl',
-      '3xl': 'h-12 w-12 text-3xl',
-      '4xl': 'h-16 w-16 text-4xl',
-      '5xl': 'h-20 w-20 text-5xl',
-      '6xl': 'h-24 w-24 text-6xl',
-      '7xl': 'h-28 w-28 text-7xl',
-      '8xl': 'h-32 w-32 text-8xl',
-      '9xl': 'h-44 w-44 text-9xl',
+      sm: 'text-sm',
+      md: 'text-base',
+      lg: 'text-lg',
+      xl: 'text-xl',
+      '2xl': 'text-2xl',
+      '3xl': 'text-3xl',
+      '4xl': 'text-4xl',
+      '5xl': 'text-5xl',
+      '6xl': 'text-6xl',
+      '7xl': 'text-7xl',
+      '8xl': 'text-8xl',
+      '9xl': 'text-9xl',
     },
   },
 });
@@ -38,7 +38,9 @@ export type UserIconProps = Omit<ComponentPropsWithoutRef<'div'>, 'children' | '
 };
 
 export const UserIcon = ({ emoji, backgroundColor, size = '3xl', ...props }: UserIconProps): ReactNode => (
-  <div className={cn('h-fit w-fit overflow-hidden rounded-full', userIconVariant({ backgroundColor }))} {...props}>
-    <span className={cn('pointer-events-none flex select-none items-center justify-center', userIconVariant({ size }))}>{emoji}</span>
+  <div className={cn('aspect-square h-fit w-fit overflow-hidden rounded-full', userIconVariant({ backgroundColor }))} {...props}>
+    <span className={cn('pointer-events-none flex select-none items-center justify-center p-2 leading-none', userIconVariant({ size }))}>
+      {emoji}
+    </span>
   </div>
 );
