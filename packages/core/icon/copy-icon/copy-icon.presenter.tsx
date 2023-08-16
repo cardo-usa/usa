3ca55aae9eadff722128bf3fb2bdca764f1a6e2d
koutyuke <gunji.kousuke08@gmail.com>
2023-08-16 20:32:04 +0900

    fix: 🐛 (copy-icon) アイコンを`PiCopyBold`から`BiCopy`へ変更

--- packages/core/icon/copy-icon/copy-icon.presenter.tsx
+++ packages/core/icon/copy-icon/copy-icon.presenter.tsx
@@ -1,6 +1,6 @@
 import type { ComponentPropsWithoutRef, ReactNode } from 'react';
-import { PiCopyBold } from 'react-icons/pi';
+import { BiCopy } from 'react-icons/bi';
 
 type CopyIconProps = Omit<ComponentPropsWithoutRef<'svg'>, 'children'>;
 
-export const CopyIcon = ({ ...props }: CopyIconProps): ReactNode => <PiCopyBold {...props} />;
+export const CopyIcon = ({ ...props }: CopyIconProps): ReactNode => <BiCopy {...props} />;
