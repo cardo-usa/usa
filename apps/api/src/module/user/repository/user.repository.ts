e4ad21575a74f9d4aa3f0c2a154b259db506afef
shio <85730998+dino3616@users.noreply.github.com>
2023-08-18 04:12:37 +0900

    feat: ✨ (user/repository) findManyByRoomId を追加

--- apps/api/src/module/user/repository/user.repository.ts
+++ apps/api/src/module/user/repository/user.repository.ts
@@ -3,6 +3,7 @@ import type { User } from '@/module/user/domain/user.model';
 
 export interface UserRepositoryInterface {
   find(userId: User['id']): Promise<User | null>;
+  findManyByRoomId(roomId: Room['id']): Promise<User[]>;
   findManyByRoomIds(roomIds: Room['id'][]): Promise<User[]>;
   create(user: Omit<User, 'id' | 'joinedAt' | 'handCards' | 'isMyTurn' | 'gameState' | 'finishedAt'>): Promise<User>;
   update(
