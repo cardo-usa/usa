9fab2b8bcca0729358e3630a06bc7107c116b648
shio <85730998+dino3616@users.noreply.github.com>
2023-08-18 00:37:06 +0900

    feat: ✨ (api/user) joiningRoom を追加

--- apps/api/src/module/user/controller/dto/object/user-object.ts
+++ apps/api/src/module/user/controller/dto/object/user-object.ts
@@ -1,12 +1,13 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { CardObject } from '@/common/dto/object/card.object';
+import { RoomObject } from '@/module/room/controller/dto/object/room-object';
 import { IconBackgroundColorEnum } from '@/module/user/controller/dto/enum/icon-background-color.enum';
 import { RoleEnum } from '@/module/user/controller/dto/enum/role.enum';
 import { UserGameStateEnum } from '@/module/user/controller/dto/enum/user-game-state.enum';
 import type { User } from '@/module/user/domain/user.model';
 
 @ObjectType('User')
-export class UserObject implements User {
+export class UserObject implements Omit<User, 'joiningRoomId'> {
   @Field(() => String, { nullable: false })
   id!: string;
 
@@ -21,6 +22,9 @@ export class UserObject implements User {
 
   @Field(() => IconBackgroundColorEnum, { nullable: false })
   iconBackgroundColor!: IconBackgroundColorEnum;
+
+  @Field(() => RoomObject, { nullable: false })
+  joiningRoom!: RoomObject;
 
   @Field(() => Date, { nullable: false })
   joinedAt!: Date;
