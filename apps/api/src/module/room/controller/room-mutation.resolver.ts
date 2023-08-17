79df4673e2c13fbd12ce1298dd2d9b91d64ead3f
shio <85730998+dino3616@users.noreply.github.com>
2023-08-18 04:47:34 +0900

    feat: ✨ (api/room/mutation) updateRoomGameState を追加

--- apps/api/src/module/room/controller/room-mutation.resolver.ts
+++ apps/api/src/module/room/controller/room-mutation.resolver.ts
@@ -1,6 +1,10 @@
 import { Inject, Logger } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { RoomUpdateGameStateInput } from './dto/input/room-update-game-state.input';
+import { RoomWhereUniqueInput } from './dto/input/room-where-unique.input';
+import { RoomObject } from './dto/object/room.object';
 import { InjectionToken } from '@/common/constant/injection-token';
+import type { Room } from '@/module/room/domain/room.model';
 import { RoomUseCaseInterface } from '@/module/room/use-case/room.use-case';
 import { UserAccountSettingInput } from '@/module/user/controller/dto/input/user-account-setting.input';
 import { UserObject } from '@/module/user/controller/dto/object/user.object';
@@ -25,5 +29,17 @@ export class RoomMutation {
     const [, createdUser] = await this.roomUseCase.createRoom(accountSetting);
 
     return createdUser;
+  }
+
+  @Mutation(() => RoomObject)
+  async updateRoomGameState(
+    @Args('where', { type: () => RoomWhereUniqueInput }) where: RoomWhereUniqueInput,
+    @Args('data', { type: () => RoomUpdateGameStateInput }) data: RoomUpdateGameStateInput,
+  ): Promise<Room> {
+    this.logger.log(`${this.updateRoomGameState.name} called`);
+
+    const updatedRoom = await this.roomUseCase.updateRoomGameState(where.id, data.gameState);
+
+    return updatedRoom;
   }
 }
