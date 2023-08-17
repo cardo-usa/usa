d6cceb6fa60bfac920a7c0d364335a70b3f8085a
koutyuke <gunji.kousuke08@gmail.com>
2023-08-17 17:01:39 +0900

    feat: ✨ (NumberCard) `NumberCard`コンポーネントの作成

---
+++ packages/core/component/card/number-card/number-card.story.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import type { ComponentPropsWithoutRef } from 'react';
+import { NumberCard } from './number-card.presenter';
+
+type Story = StoryObj<typeof NumberCard>;
+
+const meta: Meta<typeof NumberCard> = {
+  component: NumberCard,
+  argTypes: {
+    number: {
+      description: 'Number',
+      control: {
+        type: 'number',
+      },
+    },
+    color: {
+      description: 'Color',
+      control: {
+        type: 'select',
+      },
+      options: ['blue', 'red', 'green', 'yellow', 'black'] satisfies ComponentPropsWithoutRef<typeof NumberCard>['color'][],
+    },
+  },
+};
+
+export default meta;
+
+export const Default: Story = {
+  args: {
+    number: 0,
+    color: 'blue',
+  },
+};
