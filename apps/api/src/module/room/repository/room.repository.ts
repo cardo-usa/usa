f979350df64fbdecdcc5ec88e399da7fa32c3d6d
shio <85730998+dino3616@users.noreply.github.com>
2023-08-18 04:46:18 +0900

    feat: ✨ (api/room/repository) update を追加

--- apps/api/src/module/room/repository/room.repository.ts
+++ apps/api/src/module/room/repository/room.repository.ts
@@ -4,4 +4,5 @@ export interface RoomRepositoryInterface {
   find(roomId: Room['id']): Promise<Room | null>;
   findMany(roomIds: Room['id'][]): Promise<Room[]>;
   create(): Promise<Room>;
+  update(roomId: Room['id'], room: Partial<Omit<Room, 'id' | 'isWanted'>>): Promise<Room>;
 }
