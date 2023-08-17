59919acdcaca6fc6b897e91853c33e296a23da03
shio <85730998+dino3616@users.noreply.github.com>
2023-08-18 00:42:41 +0900

    feat: ✨ (api/cache) cache module を作成

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@/cache/cache.module';
 import { EnvModule } from '@/config/env/env.module';
 import { GraphQLConfigModule } from '@/config/graphql/graphql-config.module';
 import { PrismaModule } from '@/infra/prisma/prisma.module';
 import { Modules } from '@/module';
 
 @Module({
-  imports: [EnvModule, GraphQLConfigModule, PrismaModule, ...Modules],
+  imports: [EnvModule, GraphQLConfigModule, PrismaModule, CacheModule, ...Modules],
 })
 export class AppModule {}
