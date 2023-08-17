71b69a935ff411bc9417779b5a91c1271e7f678e
shio <85730998+dino3616@users.noreply.github.com>
2023-08-17 20:34:23 +0900

    feat: ✨ (constant/injection-token) room に関連する token を追加

--- apps/api/src/common/constant/injection-token.ts
+++ apps/api/src/common/constant/injection-token.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-shadow
 export enum InjectionToken {
+  ROOM_REPOSITORY = 'ROOM_REPOSITORY',
+  ROOM_USE_CASE = 'ROOM_USE_CASE',
   USER_REPOSITORY = 'USER_REPOSITORY',
   USER_USE_CASE = 'USER_USE_CASE',
 }
