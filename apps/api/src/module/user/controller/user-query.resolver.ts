a029f4c9e6ec2873c1a328cbf5af22c972a4b04b
shio <85730998+dino3616@users.noreply.github.com>
2023-08-17 14:25:05 +0900

    feat: ✨ (api/user/dto) user-query resolver を作成

---
+++ apps/api/src/module/user/controller/user-query.resolver.ts
@@ -0,0 +1,29 @@
+import { Inject, Logger } from '@nestjs/common';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { UserWhereUniqueInput } from './dto/input/user-where-unique.input';
+import { UserObject } from './dto/object/user-object';
+import { InjectionToken } from '@/common/constant/injection-token';
+import type { User } from '@/module/user/domain/user.model';
+import { UserUseCaseInterface } from '@/module/user/use-case/user.use-case';
+
+@Resolver()
+export class UserQuery {
+  private readonly logger = new Logger(UserQuery.name);
+
+  constructor(
+    @Inject(InjectionToken.USER_USE_CASE)
+    private readonly userUseCase: UserUseCaseInterface,
+  ) {}
+
+  @Query(() => UserObject, { nullable: true })
+  async findUser(
+    @Args('where', { type: () => UserWhereUniqueInput })
+    where: UserWhereUniqueInput,
+  ): Promise<User | null> {
+    this.logger.log(`${this.findUser.name} called`);
+
+    const foundUser = await this.userUseCase.find(where.id);
+
+    return foundUser;
+  }
+}
