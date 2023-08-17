07e354a3180181fee5a2b6b4548654bda33868ea
shio <85730998+dino3616@users.noreply.github.com>
2023-08-17 20:42:16 +0900

    refactor: ♻️ (api/app) modules ファイルの命名を index に修正

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { EnvModule } from '@/config/env/env.module';
 import { GraphQLConfigModule } from '@/config/graphql/graphql-config.module';
 import { PrismaModule } from '@/infra/prisma/prisma.module';
-import { Modules } from '@/module/modules';
+import { Modules } from '@/module';
 
 @Module({
   imports: [EnvModule, GraphQLConfigModule, PrismaModule, ...Modules],
