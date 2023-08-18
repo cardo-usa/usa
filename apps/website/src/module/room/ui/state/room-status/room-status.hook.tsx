ed410f40cfa0d4f4cd349e027f254f5b5c0c58bf
koutyuke <gunji.kousuke08@gmail.com>
2023-08-19 05:38:24 +0900

    feat: ⚰️ (room-state) `useRoomState`の作成

--- apps/website/src/module/room/ui/state/room-status/room-status.hook.tsx
+++ apps/website/src/module/room/ui/state/room-status/room-status.hook.tsx
@@ -1,55 +1,50 @@
-// import { useQuery, useSubscription } from '@urql/next';
-// import { useMemo } from 'react';
-// import {
-//   FindRoomDocument,
-//   UpdatedRoomDocument,
-//   type FindRoomQuery,
-//   type FindRoomQueryVariables,
-//   type UpdatedRoomSubscription,
-//   type UpdatedRoomSubscriptionVariables,
-//   type RoomWhereUniqueInput,
-// } from '#/src/infra/graphql/generated/graphql';
+import { useQuery, useSubscription } from '@urql/next';
+import { useMemo } from 'react';
+import {
+  FindRoomDocument,
+  UpdatedRoomDocument,
+  type FindRoomQuery,
+  type FindRoomQueryVariables,
+  type UpdatedRoomSubscription,
+  type UpdatedRoomSubscriptionVariables,
+} from '#/src/infra/graphql/generated/graphql';
 
-// type Props = {
-//   roomId: string;
-// };
+type Props = {
+  roomId: string;
+};
 
-// const useRoomStatus = ({ roomId }: Props) => {
-//   // Sample query
-//   const [initialResult] = useQuery<FindRoomQuery, FindRoomQueryVariables>({
-//     query: FindRoomDocument,
-//     variables: {
-//       where: { id: roomId },
-//     },
-//   });
+const useRoomStatus = ({ roomId }: Props) => {
+  const [initialResult] = useQuery<FindRoomQuery, FindRoomQueryVariables>({
+    query: FindRoomDocument,
+    variables: {
+      where: { id: roomId },
+    },
+  });
 
-//   // Sample subscription
-//   const [updatedResult] = useSubscription<UpdatedRoomSubscription, UpdatedRoomSubscriptionVariables>({
-//     query: UpdatedRoomDocument,
-//     variables: {
-//       where: { id: roomId },
-//     },
-//   });
+  const [updatedResult] = useSubscription<UpdatedRoomSubscription, UpdatedRoomSubscription, UpdatedRoomSubscriptionVariables>({
+    query: UpdatedRoomDocument,
+    variables: {
+      where: { id: roomId },
+    },
+  });
 
-//   const result = useMemo<RoomWhereUniqueInput | undefined>(() => {
-//     if (initialResult.error) {
-//       return undefined;
-//     }
-//     if (updatedResult.error) {
-//       return undefined;
-//     }
-//     if (initialResult.data !== undefined) {
-//       return initialResult.data.findRoom;
-//     }
-//     if (updatedResult.data !== undefined) {
-//       return updatedResult.data;
-//     }
-//     return undefined;
-//   }, [data, subscriptionData]);
+  const result = useMemo(() => {
+    if (initialResult.error) {
+      return undefined;
+    }
+    if (updatedResult.error) {
+      return undefined;
+    }
+    if (initialResult.data !== undefined) {
+      return initialResult.data.findRoom;
+    }
+    if (updatedResult.data !== undefined) {
+      return updatedResult.data.updatedRoom;
+    }
+    return undefined;
+  }, [initialResult, updatedResult]);
 
-//   console.log(result?.id);
+  return result;
+};
 
-//   return result;
-// };
-
-// export { useRoomStatus };
+export { useRoomStatus };
