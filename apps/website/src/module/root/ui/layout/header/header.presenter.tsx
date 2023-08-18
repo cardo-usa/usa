43e4654dd36da37d0898f0cd4910e54559eb184d
koutyuke <gunji.kousuke08@gmail.com>
2023-08-18 19:53:37 +0900

    feat: ✨ (Header) アカウント設定ができる機能の追加

--- apps/website/src/module/root/ui/layout/header/header.presenter.tsx
+++ apps/website/src/module/root/ui/layout/header/header.presenter.tsx
@@ -1,9 +1,12 @@
+'use client';
 import BrandLogo from '@usa/core/asset/brand/logo.svg';
+import { BaseDialog } from '@usa/core/component/Dialog';
 import { Image } from '@usa/core/component/image';
 import { Link } from '@usa/core/component/link';
 import { BookIcon } from '@usa/core/icon/book-icon';
 import { cn } from '@usa/tailwind';
-import type { ComponentPropsWithoutRef, ReactNode } from 'react';
+import { useState, type ComponentPropsWithoutRef, type ReactNode } from 'react';
+import { AccountSettingModal } from '@/component/modal/account-setting';
 import { UserIcon } from '@/component/user-icon';
 import type { AccountSetting } from '@/model/account-setting';
 
@@ -11,20 +14,36 @@ export type HeaderProps = Omit<ComponentPropsWithoutRef<'header'>, 'children'> &
   accountSetting: AccountSetting;
 };
 
-export const Header = ({ accountSetting, className, ...props }: HeaderProps): ReactNode => (
-  <header className={cn('flex w-screen items-center justify-between p-6', className)} {...props}>
-    <nav aria-label="main navigation">
-      <Link href="/">
-        <Image src={BrandLogo} alt="A brand logo for USA." height={48} priority className="h-12 w-auto transition hover:opacity-70" />
-        <span className="sr-only">トップ</span>
-      </Link>
-    </nav>
-    <div className="flex items-center gap-6">
-      <Link href="https://github.com/cardo-usa/usa/blob/main/doc/rule.md/" external className="group flex items-center gap-2">
-        <BookIcon className="h-8 w-8 fill-slate-12 transition group-hover:opacity-70" />
-        <span className="text-2xl font-bold text-slate-12 transition group-hover:opacity-70">Rule</span>
-      </Link>
-      <UserIcon emoji={accountSetting.iconEmoji} backgroundColor={accountSetting.iconBackgroundColor} size="3xl" />
-    </div>
-  </header>
-);
+export const Header = ({ accountSetting, className, ...props }: HeaderProps): ReactNode => {
+  const [settingOpen, setSettingOpen] = useState(false);
+  const Dialog = BaseDialog(<UserIcon emoji={accountSetting.iconEmoji} backgroundColor={accountSetting.iconBackgroundColor} size="3xl" />);
+  return (
+    <header className={cn('flex w-screen items-center justify-between p-6', className)} {...props}>
+      <nav aria-label="main navigation">
+        <Link href="/">
+          <Image src={BrandLogo} alt="A brand logo for USA." height={48} priority className="h-12 w-auto transition hover:opacity-70" />
+          <span className="sr-only">トップ</span>
+        </Link>
+      </nav>
+      <div className="flex items-center gap-6">
+        <Link href="https://github.com/cardo-usa/usa/blob/main/doc/rule.md/" external className="group flex items-center gap-2">
+          <BookIcon className="h-8 w-8 fill-slate-12 transition group-hover:opacity-70" />
+          <span className="text-2xl font-bold text-slate-12 transition group-hover:opacity-70">Rule</span>
+        </Link>
+        <Dialog open={settingOpen} setOpen={setSettingOpen}>
+          <AccountSettingModal
+            closeButtonEvent={() => {
+              setSettingOpen(false);
+            }}
+            cancelButtonEvent={() => {
+              setSettingOpen(false);
+            }}
+            submitButtonEvent={() => {
+              setSettingOpen(false);
+            }}
+          />
+        </Dialog>
+      </div>
+    </header>
+  );
+};
