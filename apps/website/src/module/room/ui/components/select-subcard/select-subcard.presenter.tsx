7c58fb5aaa63e5c99ef90e769ca0a9fd50a56de5
koutyuke <gunji.kousuke08@gmail.com>
2023-08-19 09:45:31 +0900

    feat: 🎈 (select-subcard) `ClickEvent`に必要なデータをPropsに追加

--- apps/website/src/module/room/ui/components/select-subcard/select-subcard.presenter.tsx
+++ apps/website/src/module/room/ui/components/select-subcard/select-subcard.presenter.tsx
@@ -1,18 +1,26 @@
 'use client';
 import { CardBack } from '#core/component/card/card-back';
 import { NumberCard } from '#core/component/card/number-card';
+import { useMutation } from '@urql/next';
 import { useState, type FC, type Dispatch, type SetStateAction } from 'react';
+import {
+  PutHandCardToFieldCardsDocument,
+  type PutHandCardToFieldCardsMutation,
+  type PutHandCardToFieldCardsMutationVariables,
+} from '#/src/infra/graphql/generated/graphql';
 
 type Props = {
   subcard: number[];
   closeValue: boolean;
   closeSetter: Dispatch<SetStateAction<boolean>>;
-  selectEvent: (value: number) => void;
+  handCardId: string;
+  accountId: string;
 };
 
-const SelectSubcard: FC<Props> = ({ subcard, closeSetter, closeValue, selectEvent }) => {
+const SelectSubcard: FC<Props> = ({ subcard, closeSetter, closeValue, handCardId, accountId }) => {
   const [isSelect, setIsSelect] = useState(false);
   const [selectValue, setSelectValue] = useState<number | null>(null);
+  const [, setPutCard] = useMutation<PutHandCardToFieldCardsMutation, PutHandCardToFieldCardsMutationVariables>(PutHandCardToFieldCardsDocument);
 
   return (
     <div className="absolute left-0 top-0 z-10 flex h-screen w-screen flex-col items-center justify-end space-y-12 bg-black-9 p-8">
@@ -26,7 +34,11 @@ const SelectSubcard: FC<Props> = ({ subcard, closeSetter, closeValue, selectEven
               if (!isSelect) {
                 setIsSelect(true);
                 setSelectValue(value);
-                selectEvent(value);
+                setPutCard({
+                  where: { id: accountId },
+                  handCardId: handCardId,
+                  subCard: value,
+                });
                 window.setTimeout(() => {
                   closeSetter(closeValue);
                 }, 1500);
